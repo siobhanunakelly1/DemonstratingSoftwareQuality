97f373bcaf1d242c3b180433429d53ead7d51bce
Siobhan Kelly <siobhanunakelly1@gmail.com>
2021-05-20 01:14:29 +0100

    created Rubric & Criterion Classes

---
+++ Rubric/src/Criterion.java
@@ -0,0 +1,15 @@
+public class Criterion {
+    private String criterionName;
+
+    public Criterion(String name){
+        this.criterionName = name;
+    }
+
+    public String getCriterion() {
+        return criterionName;
+    }
+
+    public void setCriterion(String criterion) {
+        this.criterionName = criterion;
+    }
+}
---
+++ Rubric/src/Rubric.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Rubric {
+    private String subject;
+    private ArrayList<Criterion> criteria = new ArrayList<>();
+
+    public Rubric(String subject){
+        this.subject = subject;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public ArrayList<Criterion> getCriteria() {
+        return criteria;
+    }
+
+    public void setCriteria(ArrayList<Criterion> criteria) {
+        this.criteria = criteria;
+    }
+
+    public void addCriterion(Criterion criterion){
+        this.criteria.add(criterion);
+    }
+
+}

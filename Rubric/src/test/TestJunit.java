952fe99d6b1dcc535eec0d6169334a1f97a439c6
Siobhan Kelly <siobhanunakelly1@gmail.com>
2021-05-20 15:30:26 +0100

    add Criterion to Rubric

--- Rubric/src/test/TestJunit.java
+++ Rubric/src/test/TestJunit.java
@@ -1,9 +1,11 @@
 package test;
 
 import main.Controller;
+import main.Criterion;
 import main.Rubric;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -16,5 +18,15 @@ public class TestJunit {
         assertEquals("Maths", r.getSubject());
 
     }
+
+    @Test
+    public void testAddCriterion(){
+        Controller c = new Controller();
+        Rubric r = c.createRubric("Maths");
+        Criterion cr = new Criterion("Algebra");
+        r.addCriterion(cr);
+        assertTrue(r.getCriteria().contains(cr));
+    }
+
     
 }
